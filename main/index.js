1add7ea517c4a9799ece1f0e8af5e1b4e7874206
itztiva <98614842+itztiva@users.noreply.github.com>
2024-03-29 15:31:10 -0400

    more debug

--- main/index.js
+++ main/index.js
@@ -7,11 +7,38 @@ require('dotenv').config({ path: path.resolve(__dirname, 'config', '.env') });
 
 const port = process.env.PORT
 
+express.use((req, res, next) => {
+    log.backend(`${req.method} ${req.originalUrl}`);
+    next();
+});
+
 express.get("/", async (req, res) => {
     res.json({
         message: "Moonlight is running!"
     });
 });
+
+express.use(async (req, res, next) => {
+    log.backend("Error on:" + req.path);
+    const errorname = "error.Moonlight.common.not_found";
+    const errorcode = 1004;
+  
+    res.set({
+      'X-Epic-Error-Name': errorname,
+      'X-Epic-Error-Code': errorcode
+    });
+  
+    res.status(404);
+    res.json({
+      "errorCode": errorcode,
+      "errorname": errorname,
+      "errorMessage": "Not found.",
+      "numericErrorCode": errorcode,
+      "originatingService": "any",
+      "intent": "prod"
+    });
+    res.end();
+  });
 
 fs.readdirSync("./main/operations").forEach(file => {
     const route = require(path.join(__dirname, "operations", file));
