83b24b6549460a77b5549c90e22cfae5cec72ef6
itztiva <98614842+itztiva@users.noreply.github.com>
2024-03-29 15:15:05 -0400

    contentpages

---
+++ main/operations/contentpages.js
@@ -0,0 +1,13 @@
+const Express = require("express")
+const express = Express();
+const log = require("../utils/base/log.js")
+const functions = require("../utils/functions/functions.js")
+const fs = require("fs");
+const path = require("path");
+
+express.get("/content/api/pages/fortnite-game", async (req,res) => {
+    const c = functions.contentpages(req);
+    res.json(c);
+});
+
+module.exports = express
--- main/utils/functions/functions.js
+++ main/utils/functions/functions.js
@@ -1,4 +1,117 @@
-function createError(errorCode, errorMessage, messageVars, numericErrorCode, error, statusCode, res) {
+const getVersion = (req) => {
+    let memory = {
+        season: 0,
+        build: 0.0,
+        CL: "0",
+        lobby: ""
+    };
+    if (req && req.headers && req.headers["user-agent"]) {
+        let CL = "";
+        try {
+            let BuildID = req.headers["user-agent"].split("-")[3].split(",")[0];
+            if (!Number.isNaN(Number(BuildID)))
+                CL = BuildID;
+            else {
+                BuildID = req.headers["user-agent"].split("-")[3].split(" ")[0];
+                if (!Number.isNaN(Number(BuildID)))
+                    CL = BuildID;
+            }
+        }
+        catch {
+            try {
+                let BuildID = req.headers["user-agent"].split("-")[1].split("+")[0];
+                if (!Number.isNaN(Number(BuildID)))
+                    CL = BuildID;
+            }
+            catch { }
+        }
+        try {
+            let Build = req.headers["user-agent"].split("Release-")[1].split("-")[0];
+            if (Build.split(".").length == 3) {
+                let Value = Build.split(".");
+                Build = Value[0] + "." + Value[1] + Value[2];
+            }
+            memory.season = Number(Build.split(".")[0]);
+            memory.build = Number(Build);
+            memory.CL = CL;
+            memory.lobby = `LobbySeason${memory.season}`;
+            if (Number.isNaN(memory.season))
+                throw new Error();
+        }
+        catch (e) {
+            if (Number.isNaN(memory.CL)) {
+                memory.season = 0;
+                memory.build = 0.0;
+                memory.CL = CL;
+                memory.lobby = "LobbySeason0";
+            }
+            else if (Number(memory.CL) < 3724489) {
+                memory.season = 0;
+                memory.build = 0.0;
+                memory.CL = CL;
+                memory.lobby = "LobbySeason0";
+            }
+            else if (Number(memory.CL) <= 3790078) {
+                memory.season = 1;
+                memory.build = 1.0;
+                memory.CL = CL;
+                memory.lobby = "LobbySeason1";
+            }
+            else {
+                memory.season = 2;
+                memory.build = 2.0;
+                memory.CL = CL;
+                memory.lobby = "LobbyWinterDecor";
+            }
+        }
+    }
+    return memory;
+};
+
+const contentpages = (req) => {
+    const ver = getVersion(req);
+    const c = require("../../../local/resources/c.json");
+
+    try {
+        c.dynamicbackgrounds.backgrounds.backgrounds[0].stage = `season${ver.season}`;
+        c.dynamicbackgrounds.backgrounds.backgrounds[1].stage = `season${ver.season}`;
+
+        if (ver.season == 10) {
+            c.dynamicbackgrounds.backgrounds.backgrounds[0].stage = "seasonx";
+            c.dynamicbackgrounds.backgrounds.backgrounds[1].stage = "seasonx";
+        }
+
+        if (ver.build == 11.31 || ver.build == 11.40) {
+            c.dynamicbackgrounds.backgrounds.backgrounds[0].stage = "Winter19";
+            c.dynamicbackgrounds.backgrounds.backgrounds[1].stage = "Winter19";
+        }
+
+        if (ver.build == 19.01) {
+            c.dynamicbackgrounds.backgrounds.backgrounds[0].stage = "winter2021";
+            c.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn.discordapp.com/attachments/927739901540188200/930880158167085116/t-bp19-lobby-xmas-2048x1024-f85d2684b4af.png";
+            c.subgameinfo.battleroyale.image = "https://cdn.discordapp.com/attachments/927739901540188200/930880421514846268/19br-wf-subgame-select-512x1024-16d8bb0f218f.jpg";
+            c.specialoffervideo.bSpecialOfferEnabled = true; 
+        }
+
+        if (ver.season == 20) {
+            if (ver.build == 20.40) {
+                c.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn2.unrealengine.com/t-bp20-40-armadillo-glowup-lobby-2048x2048-2048x2048-3b83b887cc7f.jpg";
+            } else {
+                c.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn2.unrealengine.com/t-bp20-lobby-2048x1024-d89eb522746c.png";
+            }
+        }
+
+        if (ver.season == 21) {
+            c.dynamicbackgrounds.backgrounds.backgrounds[0].backgroundimage = "https://cdn2.unrealengine.com/s21-lobby-background-2048x1024-2e7112b25dc3.jpg";
+        }
+    } catch (error) {
+        console.error("Error in contentpages function:", error);
+    }
+    
+    return c;
+}
+
+const createError = (errorCode, errorMessage, messageVars, numericErrorCode, error, statusCode, res) => {
     if (res) {
         res.set({
             'X-Epic-Error-Name': errorCode,
@@ -18,9 +131,10 @@ function createError(errorCode, errorMessage, messageVars, numericErrorCode, err
     } else {
         // idk yet
     }
-}
+};
 
-
-module.exports = [
+module.exports = {
+    getVersion,
+    contentpages,
     createError
-]
+};
